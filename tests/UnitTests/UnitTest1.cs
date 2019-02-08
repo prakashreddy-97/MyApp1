f13d13dc841c37c88b9a61c1039b45d97a0df8f0
prakash <mamidisaiprakash@gmail.com>
2019-02-08 16:34:21 -0600

    testcase changed

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -16,6 +16,9 @@ namespace UnitTests
             int actual = add(x, y);
             Assert.Equal(expected, actual);
         }
+        public void checkValue(int expected, int a, int b){
+            Assert.Equal(expected,add(a,b));
+        }
         [Theory]
         [InlineData(15,7,8)]
         [InlineData(20,15,5)]
